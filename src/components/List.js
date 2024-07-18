ec98d62319ebda7866f6b075e1028b053167deb1
Juhyuk-Park <tlsoshy@ajou.ac.kr>
2024-07-18 21:09:25 +0900

    삭제,  체크 메서드 위치 변경

--- src/components/List.js
+++ src/components/List.js
@@ -2,34 +2,11 @@ import ListItem from "./ListItem";
 import styles from "./List.module.css";
 
 function List({ toDos, setToDos }) {
-  const onDeleteClick = (e) => {
-    if (window.confirm("할일을 삭제하시겠습니까?")) {
-      const id = parseInt(e.target.parentElement.parentElement.id);
-      setToDos(toDos.filter((toDo, index) => index !== id));
-    }
-  };
-
-  const onCheckClick = (e) => {
-    const id = parseInt(e.target.parentElement.parentElement.id);
-    setToDos(
-      toDos.map((toDo, index) =>
-        index === id ? { text: toDo.text, checked: !toDo.checked } : toDo
-      )
-    );
-  };
   return (
     <ul className={styles.list}>
       {toDos.map((toDo, index) => (
         <li key={index} id={index}>
-          <ListItem
-            id={index}
-            text={toDo.text}
-            checked={toDo.checked}
-            toDos={toDos}
-            setToDos={setToDos}
-            onCheckClick={onCheckClick}
-            onDeleteClick={onDeleteClick}
-          />
+          <ListItem id={index} toDo={toDo} toDos={toDos} setToDos={setToDos} />
         </li>
       ))}
     </ul>
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,34 +2,44 @@ import { useState } from "react";
 import styles from "./ListItem.module.css";
 import UpdateInput from "./UpdateInput";
 
-function ListItem({
-  id,
-  text,
-  checked,
-  toDos,
-  setToDos,
-  onCheckClick,
-  onDeleteClick,
-}) {
+function ListItem({ id, toDo, toDos, setToDos }) {
   const [upInput, setUpInput] = useState(false);
   const onUpdateClick = () => {
     setUpInput(!upInput);
   };
+
+  const onDeleteClick = (e) => {
+    if (window.confirm("할일을 삭제하시겠습니까?")) {
+      const id = parseInt(e.target.parentElement.parentElement.id);
+      setToDos(toDos.filter((_, index) => index !== id));
+    }
+  };
+
+  const onCheckClick = (e) => {
+    const id = parseInt(e.target.parentElement.parentElement.id);
+    setToDos(
+      toDos.map((toDo, index) =>
+        index === id ? { text: toDo.text, checked: !toDo.checked } : toDo
+      )
+    );
+  };
   return (
     <div className={styles.list_item}>
-      <button onClick={onCheckClick}>{checked ? "✅" : "⬜"}</button>
+      <button onClick={onCheckClick}>{toDo.checked ? "✅" : "⬜"}</button>
       <span className={styles.todo_content}>
         {upInput ? (
           <UpdateInput
             id={id}
-            text={text}
-            checked={checked}
+            text={toDo.text}
+            checked={toDo.checked}
             toDos={toDos}
             setToDos={setToDos}
             setUpInput={setUpInput}
           />
         ) : (
-          <span className={checked ? styles.line_through : ""}>{text}</span>
+          <span className={toDo.checked ? styles.line_through : ""}>
+            {toDo.text}
+          </span>
         )}
       </span>
       <button className={styles.update_button} onClick={onUpdateClick}>
