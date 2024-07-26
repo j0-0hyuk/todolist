ae9176962c567716c72999bf39a4899cff00d1fa
Juhyuk-Park <tlsoshy@ajou.ac.kr>
2024-07-26 12:55:23 +0900

    로컬스토리지에 toDos 저장

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import CreateInput from "./components/CreateInput";
 import List from "./components/List";
 
+const TODOS_KEY = "todos";
+
 function App() {
-  const [toDos, setToDos] = useState([]);
+  const savedToDos = localStorage.getItem(TODOS_KEY) || "[]";
+  const [toDos, setToDos] = useState(JSON.parse(savedToDos));
+
+  useEffect(
+    () => localStorage.setItem(TODOS_KEY, JSON.stringify(toDos)),
+    [toDos]
+  );
 
   return (
     <div className={styles.container}>
